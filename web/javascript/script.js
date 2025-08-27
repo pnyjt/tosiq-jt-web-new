379fc3152668e2761971d504d80093bfbd4baf03
Hafiz Furqan Irfan <43038504+leofurqan@users.noreply.github.com>
2025-08-27 17:45:44 +0500

    js basic, template 1 for design

---
+++ web/javascript/script.js
@@ -0,0 +1,28 @@
+// ********* DATA TYPE
+// number: 12345
+// float: 12.3
+// boolean: true, false
+// string: "some text"
+// array: myarray = ['Apple', 'Orange']; myarray[0]
+// object: user =  {name: 'Asad', contactno: '123'}; user.name
+// typeof: tell value type
+var a = 10;
+var b = 20;
+var sum = a + b;
+// document.write(sum);
+// document.getElementById("result").innerText = sum
+// console.log(sum);
+// alert(sum);
+// x = 5; y = 10
+// x + y + 3 + (x + y)
+// var txt = "This is sample text";
+// var no = 123;
+// var output = no + txt;
+
+function getResult() {
+  var subject1 = 30;
+  var subject2 = 23;
+  var subject3 = 45;
+  var result = (subject1 + subject2 + subject3) / 3;
+  document.getElementById("result").innerText = "Average marks: " + result;
+}

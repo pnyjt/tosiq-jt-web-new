7c8f58e5f5d3c7afb1722bfdb05331b2616b943a
Hafiz Furqan Irfan <43038504+leofurqan@users.noreply.github.com>
2025-09-03 16:50:06 +0500

    Theme color

---
+++ web/assets/js/page1.js
@@ -0,0 +1,12 @@
+function changeBg(){
+    var color = document.getElementById("bg-color").value;
+    // console.log(color);
+    document.body.style.backgroundColor = color;
+}
+
+function changeTextColor(){
+
+    var color = document.getElementById("text-color").value;
+    // console.log(color);
+    document.body.style.color = color;
+}

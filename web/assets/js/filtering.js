10fbe68b1e7528bc230012d74ac18ba7f11e7974
Hafiz Furqan Irfan <43038504+leofurqan@users.noreply.github.com>
2025-09-10 17:38:30 +0500

    map, array function

--- web/assets/js/filtering.js
+++ web/assets/js/filtering.js
@@ -9,11 +9,22 @@ const users = [
 ];
 
 let userRole = "vendor";
+
 function check (users) {
     if (users.role == userRole) {
         return users;
     }
 }
-let result = users.filter(check);
+// let result = users.filter(check);
+let result = users.find(check);
 console.log(result);
 
+let arr = ["Fruits", "Foods", "Sports", "Events"]
+arr.splice(1, 0, "Lahore", "Karachi", "Faisalabad");
+console.log(arr);
+let arr2 = ["Phone case", "USB drive", "Ram"];
+console.log([...arr, ...arr2]);
+
+let myArray = [2,3,4,5,6,7,8,9];
+let output = users.map( (element) => { return element } );
+console.log(output);

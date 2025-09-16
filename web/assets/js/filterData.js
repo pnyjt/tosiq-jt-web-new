138aef0d68999b9c0cc4799d8879bb7193b74f54
Hafiz Furqan Irfan <43038504+leofurqan@users.noreply.github.com>
2025-09-16 17:43:16 +0500

    Filter dynamic table content jquery

--- web/assets/js/filterData.js
+++ web/assets/js/filterData.js
@@ -1,4 +1,24 @@
+const users = [
+    {id: 101, firstName: "Ali", lastName: "Usman", email: "ali@mail.com"},
+    {id: 102, firstName: "John", lastName: "Alex", email: "john@mail.com"},
+];
+
 $(document).ready(function(){
+
+    showData();
+    function showData(){
+        users.forEach((user, i) => {
+            $("#myTable").append(`
+                <tr>
+                    <td> ${user.id} </td>
+                    <td> ${user.firstName} </td>
+                    <td> ${user.lastName} </td>
+                    <td> ${user.email} </td>
+                </tr>
+                `)
+        })
+    }
+
     $("#myInput").on("input", function (){
         let value = $(this).val().toLowerCase();
         $("#myTable tr").filter(function (){

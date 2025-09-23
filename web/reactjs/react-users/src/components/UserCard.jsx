0b118c357d6b6215a0559be6fc693a88074a22d8
Hafiz Furqan Irfan <43038504+leofurqan@users.noreply.github.com>
2025-09-23 17:17:06 +0500

    Little changes

--- web/reactjs/react-users/src/components/UserCard.jsx
+++ web/reactjs/react-users/src/components/UserCard.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import { FaMessage, FaUserPlus  } from "react-icons/fa6";
+import { LuMessageSquareShare, LuUserPlus } from "react-icons/lu";
 import noImg from '../assets/images/users/no-img.png';
 
 const UserCard = ({ name, profile, username }) => {
   return (
     <div className='card'>
-        <div className='profile-img'>
-          <img src={profile == null ? noImg : profile} alt="" />
-        </div>
-        <h3 className='text-2xl'>{name}</h3>
-        <p>{username}</p>
-        <button> 
-          <FaMessage className='inline mt-[-3px]' /> Send Message
-        </button> {" "}
-        <button>
-          <FaUserPlus className='inline mt-[-3px]' /> Add Friend
-        </button>
+      <div className='profile-img'>
+        <img src={profile == null ? noImg : profile} alt="" />
+      </div>
+      <h3 className='text-2xl'>{name}</h3>
+      <p>{username}</p>
+      <button>
+        <LuMessageSquareShare className='inline mt-[-3px]' />
+      </button>
+      <span className='mx-2 border-l-1 border-l-gray-300'></span>
+      <button>
+        <LuUserPlus className='inline mt-[-3px]' />
+      </button>
     </div>
   )
 }

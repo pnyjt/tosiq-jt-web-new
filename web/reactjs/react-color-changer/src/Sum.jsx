cfade87efc2de0a9cab725abe76ab6598651735c
Hafiz Furqan Irfan <43038504+leofurqan@users.noreply.github.com>
2025-09-29 17:20:43 +0500

    React updated useRef()

--- web/reactjs/react-color-changer/src/Sum.jsx
+++ web/reactjs/react-color-changer/src/Sum.jsx
@@ -1,32 +1,32 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 const Sum = () => {
     const no1 = useRef(null);
     const no2 = useRef(null);
 
+    const [val, setVal] = useState("")
+
     const printSum = () => {
-        
         let a = no1.current.value;
         let b = no2.current.value;
-        
         if (a.length == 0 || b.length == 0) {
             alert("Please enter values");
             return;
         };
-
         let sum = Number(a) + Number(b);
         console.log(`Sum is: ${sum}`)
+        setVal(sum)
     }
   return (
     <div>
         <h1>Sum of two numbers</h1>
         <label>Number 1: </label>
         <input type="number" ref={no1} placeholder='Enter value a'/> <br /><br />
-        
         <label>Number 2: </label>
         <input type="number" ref={no2} placeholder='Enter value b'/> <br /><br />
-
         <button onClick={printSum}>Print Sum</button>
+
+        <div>Sum is: {val}</div>
     </div>
   )
 }
